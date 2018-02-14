f094fd75a9e26892a738ef3756a1bdd21e126ffe
Nicolás Duarte <nicolas.duarte@infinixsoft.com>
2018-02-14 16:39:57 -0300

    Change logo clinics

--- ClinicsAndDoctors/app/src/main/java/com/clinicsanddoctors/ui/aboutUs/AboutUsFragment.java
+++ ClinicsAndDoctors/app/src/main/java/com/clinicsanddoctors/ui/aboutUs/AboutUsFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.clinicsanddoctors.R;
 import com.clinicsanddoctors.ui.BaseClinicFragment;
@@ -19,7 +21,16 @@ public class AboutUsFragment extends BaseClinicFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        ImageView image = (ImageView) view.findViewById(R.id.mAboutUsText);
+
+        String language = getResources().getConfiguration().locale.getLanguage();
+        if(language.equalsIgnoreCase("ar"))
+            image.setImageResource(R.drawable.about_us_arabe);
+        else
+            image.setImageResource(R.drawable.about_us_eng);
+
+        return view;
     }
 
 }
